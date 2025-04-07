3142bf5fac32917bdc61354d45c48a5145b68e85
dev-lore25 <llorena.ct@gmail.com>
2025-04-07 03:05:45 +0000

    Lendo dados da carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -16,29 +16,47 @@ int main() {
     float pib1,pib2;
     int pontosTuristicos1,pontosTuristicos2;
 
-    printf("Digite o estado da carta 01: \n");
+    printf("Digite o estado da carta 1: \n");
     scanf ("%c", &estado1);
 
-    printf("Digite o codigo da carta 01: \n");
+    printf("Digite o codigo da carta 1: \n");
     scanf ("%s", &codigoCarta1);
 
-    printf("Digite o nome da cidade 01: \n");
+    printf("Digite o nome da cidade 1: \n");
     scanf("%s", &nomeCidade1);
 
-    printf("Digite o nome da populaçao 01: \n");
+    printf("Digite o nome da populaçao 1: \n");
     scanf("%d", &populaçao1);
 
-    printf("Digite o nome da area 01: \n");
+    printf("Digite o nome da area 1: \n");
     scanf("%f", &area1);
 
-    printf("Digite o nome do pib 01: \n");
+    printf("Digite o nome do pib 1: \n");
     scanf("%f", &pib1);
 
-    printf("Digite o nome dos pontos turisticos 01: \n");
+    printf("Digite o nome dos pontos turisticos 1: \n");
     scanf("%d", &pontosTuristicos1);
 
+    printf("Digite o nome do estado 2: \n");
+    scanf("%c", &estado2);
 
+    printf("Digite o codigo da carta 2: \n");
+    scanf ("%s", &codigoCarta2);
+    
+    printf("Digite o nome da cidade 2: \n");
+    scanf("%s", &nomeCidade2);
 
+    printf("Digite o nome da populaçao 2: \n");
+    scanf("%d", &populaçao2);
+
+    printf("Digite o nome da area 2: \n");
+    scanf("%f", &area2);
+
+    rintf("Digite o nome do pib 2: \n");
+    scanf("%f", &pib2);
+
+    printf("Digite o nome dos pontos turisticos 2: \n");
+    scanf("%d", &pontosTuristicos2);
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
